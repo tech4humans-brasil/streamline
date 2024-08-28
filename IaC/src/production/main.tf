588d83af341427ec1500bc5503dd055c7c82020a
big-ricardo <rcsrcs1569@gmail.com>
2024-08-28 10:16:05 -0300

    chore: Update Terraform version to 1.7.5 and add backup and capacity settings for Cosmos DB

--- IaC/src/production/main.tf
+++ IaC/src/production/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.97.1"
     }
   }
@@ -11,6 +11,14 @@ provider "azurerm" {
   features {}
   subscription_id = "36f0c050-1087-4637-8d2c-69624bcd2807"
 }
+
+locals {
+  cosmos_connection_string = azurerm_cosmosdb_account.cosmos.connection_strings[0]
+  cosmos_connection_parts  = split("?", local.cosmos_connection_string)
+  MONGO_URI                = local.cosmos_connection_parts[0]
+  MONGO_PARAMS             = length(local.cosmos_connection_parts) > 1 ? local.cosmos_connection_parts[1] : ""
+}
+
 
 resource "azurerm_resource_group" "rg" {
   name     = "sbus-rg"
@@ -78,32 +86,8 @@ resource "azurerm_servicebus_queue" "interaction" {
   ]
 }
 
-resource "azurerm_servicebus_queue" "evaluated" {
-  name                = "dev-evaluated"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  enable_partitioning = false
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
 resource "azurerm_servicebus_queue" "interaction_process" {
   name                = "dev-interaction_process"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  enable_partitioning = false
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
-resource "azurerm_servicebus_queue" "evaluation_process" {
-  name                = "dev-evaluation_process"
   namespace_id = azurerm_servicebus_namespace.sbus.id
   enable_partitioning = false
   max_delivery_count = 10
@@ -150,7 +134,7 @@ resource "azurerm_static_web_app" "static" {
   location            = azurerm_resource_group.rg_static.location
   sku_tier = "Free"
   sku_size = "Free"
-
+  
   depends_on = [
     azurerm_resource_group.rg_static
   ]
@@ -200,6 +184,16 @@ resource "azurerm_cosmosdb_account" "cosmos" {
   name                = "dev-streamline-mongo"
   offer_type          = "Standard"
   resource_group_name = azurerm_resource_group.rg_mongo.name
+  backup {
+    type = "Periodic"
+    storage_redundancy = "Local"
+    interval_in_minutes         = 240     
+    retention_in_hours          = 8        
+  }
+  minimal_tls_version = "Tls12"
+  capacity {
+    total_throughput_limit = 500
+  }
   tags = {
     defaultExperience       = "Azure Cosmos DB for MongoDB API"
     hidden-cosmos-mmspecial = ""
@@ -211,7 +205,6 @@ resource "azurerm_cosmosdb_account" "cosmos" {
     failover_priority = 0
     location          = "eastus2"
   }
-  enable_free_tier = true
 
   depends_on = [
     azurerm_resource_group.rg_mongo
@@ -220,30 +213,34 @@ resource "azurerm_cosmosdb_account" "cosmos" {
 
 
 resource "azurerm_linux_function_app" "func1" {
-  name                       = "streamline-services"
+  name                       = "dev-streamline-services"
   resource_group_name = azurerm_resource_group.rg_functions.name
   location            = azurerm_resource_group.rg_functions.location
 
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
   service_plan_id            = azurerm_service_plan.ASP-functionsrg-a025.id
+  https_only = true
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "node",
     "AZURE_SERVICE_BUS_CONNECTION_STRING" = azurerm_servicebus_namespace.sbus.default_primary_connection_string,
-    AZURE_STORAGE_CONNECTION_STRING = azurerm_storage_account.storage.primary_connection_string,
-    "MONGO_URI": azurerm_cosmosdb_account.cosmos.connection_strings[0],
-    "JWT_SECRET": "",
-    "JWT_RESET_PASSWORD_SECRET": "",
-    "FRONTEND_URL": "",
+    "AZURE_STORAGE_CONNECTION_STRING" = azurerm_storage_account.storage.primary_connection_string,
+    "MONGO_URI"                          = local.MONGO_URI,
+    "MONGO_PARAMS"                       = local.MONGO_PARAMS,
+    "JWT_SECRET": "UbF6O#71K5(",
+    "JWT_RESET_PASSWORD_SECRET": "UbF6O#71K5(2",
+    "FRONTEND_URL": "streamline.hml-tech4h.com.br",
     "EMAIL_ACCOUNT": "noreply@hml-tech4h.com.br",
-    "SENDGRID_API_KEY": "",
+    "SENDGRID_API_KEY": "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",
     "SENTRY_DSN" = "",
+    "FRONTEND_URL": "streamline.hml-tech4h.com.br",
+    "LOGGING": "false",
     NODE_ENV = "development"
   }
   site_config {
     cors {
-      allowed_origins     = ["*"]
+      allowed_origins     = [azurerm_static_web_app.static.default_host_name, "streamline.hml-tech4h.com.br"]
       support_credentials = false
     }
     application_stack {
@@ -255,5 +252,6 @@ resource "azurerm_linux_function_app" "func1" {
     azurerm_storage_account.storage,
     azurerm_servicebus_namespace.sbus,
     azurerm_cosmosdb_account.cosmos,
+    azurerm_static_web_app.static,
   ]
 }
