4a48ef8c6eadec4cb2c655aaa3b723435e38e4ba
big-ricardo <rcsrcs1569@gmail.com>
2024-09-25 16:43:44 -0300

    refactor: Remove unused import statements in Form, Project, and Workflow components

--- Frontend/src/pages/Portal/Forms/Form.tsx
+++ Frontend/src/pages/Portal/Forms/Form.tsx
@@ -4,7 +4,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
-  Box,
   Button,
   Card,
   CardBody,
