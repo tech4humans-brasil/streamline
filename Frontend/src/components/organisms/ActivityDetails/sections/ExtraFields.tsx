69ff17cd658fddd4b78e5b63f12cbe8a14c6e75d
big-ricardo <rcsrcs1569@gmail.com>
2024-10-18 13:58:27 -0300

    refactor: Update ExtraFields component in ActivityDetails section
    - Add useMemo hook to optimize rendering of fields with values
    - Add useMemo hook to optimize rendering of fields without values
    - Update rendering logic to use filtered fields arrays
    - Add conditional rendering for empty fields section
    - Update conditional rendering logic for empty fields section

--- Frontend/src/components/organisms/ActivityDetails/sections/ExtraFields.tsx
+++ Frontend/src/components/organisms/ActivityDetails/sections/ExtraFields.tsx
@@ -1,5 +1,5 @@
 // ExtraFields.tsx
-import React from "react";
+import React, { useMemo } from "react";
 import { Flex } from "@chakra-ui/react";
 import RenderFieldValue from "@components/atoms/RenderFieldValue";
 import { IField } from "@interfaces/FormDraft";
@@ -9,29 +9,39 @@ interface ExtraFieldsProps {
   fields: IField[];
 }
 
-const ExtraFields: React.FC<ExtraFieldsProps> = ({ fields }) => {
+const ExtraFields: React.FC<ExtraFieldsProps> = ({ fields = [] }) => {
+  const fieldsFilled = useMemo(
+    () => fields.filter((field) => field?.value),
+    [fields]
+  );
+
+  const fieldsEmpty = useMemo(
+    () => fields.filter((field) => !field?.value),
+    [fields]
+  );
+
   return (
     <Flex flexWrap="wrap" gap={4} direction={"column"}>
-      {fields
-        .filter((field) => field.value)
-        .map((field) => (
-          <RenderFieldValue key={field.id} field={field} />
-        ))}
+      {fieldsFilled.map((field) => (
+        <RenderFieldValue key={field.id} field={field} />
+      ))}
 
-      <Accordion.Container allowToggle allowMultiple defaultIndex={[]}>
-        <Accordion.Item>
-          <Accordion.Button fontSize="sm">
-            Campos não preenchidos
-          </Accordion.Button>
-          <Accordion.Panel>
-            {fields
-              .filter((field) => !field.value)
-              .map((field) => (
-                <RenderFieldValue key={field.id} field={field} />
-              ))}
-          </Accordion.Panel>
-        </Accordion.Item>
-      </Accordion.Container>
+      {fieldsEmpty.length > 0 && (
+        <Accordion.Container allowToggle allowMultiple defaultIndex={[]}>
+          <Accordion.Item>
+            <Accordion.Button fontSize="sm">
+              Campos não preenchidos
+            </Accordion.Button>
+            <Accordion.Panel>
+              {fields
+                .filter((field) => !field.value)
+                .map((field) => (
+                  <RenderFieldValue key={field.id} field={field} />
+                ))}
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion.Container>
+      )}
 
       {fields.length === 0 && (
         <Flex justifyContent="center" alignItems="center" h="100%">
