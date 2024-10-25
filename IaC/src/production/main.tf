b62189bfcfc725d11e810660737af81b9fa5ec5a
big-ricardo <rcsrcs1569@gmail.com>
2024-10-25 14:56:03 -0300

    refactor: Update .gitignore and .github/workflows files

--- IaC/src/production/main.tf
+++ IaC/src/production/main.tf
@@ -9,256 +9,170 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "36f0c050-1087-4637-8d2c-69624bcd2807"
+  subscription_id = "76a1e361-6171-48aa-b143-27f24f8aacde"
 }
-
-locals {
-  MONGO_URI                = ""
-  MONGO_PARAMS             = ""
-}
-
 
 resource "azurerm_resource_group" "rg" {
-  name     = "sbus-rg"
-  location = "eastus2"
+  name     =var.RESOURCE_GROUP
+  location =var.LOCATION
 }
 
 resource "azurerm_servicebus_namespace" "sbus" {
-  name                = "dev-streamline-sbus"
+  name                = "prod-streamline-sbus"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Basic"
   minimum_tls_version = "1.2"
   public_network_access_enabled = true
-
-  depends_on = [
-    azurerm_resource_group.rg
-  ]
 }
 
-resource "azurerm_servicebus_queue" "send_email" {
-  name                = "send_email"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
-resource "azurerm_servicebus_queue" "change_status" {
-  name                = "change_status"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
-resource "azurerm_servicebus_queue" "swap_workflow" {
-  name                = "swap_workflow"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
-resource "azurerm_servicebus_queue" "interaction" {
-  name                = "interaction"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
-resource "azurerm_servicebus_queue" "interaction_process" {
-  name                = "interaction_process"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
-resource "azurerm_servicebus_queue" "web_request" {
-  name                = "web_request"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
-
-resource "azurerm_servicebus_queue" "conditional" {
-  name                = "conditional"
-  namespace_id = azurerm_servicebus_namespace.sbus.id
-  max_delivery_count = 10
-  lock_duration = "PT5M"
-
-  depends_on = [
-    azurerm_servicebus_namespace.sbus
-  ]
-}
-
-resource "azurerm_resource_group" "rg_mongo" {
-  name     = "static-data-rg"
-  location = "eastus2"
-}
-
-resource "azurerm_resource_group" "rg_static" {
-  name     = "static-web-app-rg"
-  location = "eastus2"
-  
+resource "azurerm_servicebus_queue" "queues" {
+  count               = 7
+  name                = element(["send_email", "change_status", "swap_workflow", "interaction", "interaction_process", "web_request", "conditional"], count.index)
+  namespace_id        = azurerm_servicebus_namespace.sbus.id
+  max_delivery_count  = 2
+  lock_duration       = "PT5M"
 }
 
 resource "azurerm_static_web_app" "static" {
-  name                = "dev-streamline"
-  resource_group_name = azurerm_resource_group.rg_static.name
-  location            = azurerm_resource_group.rg_static.location
-  sku_tier = "Free"
-  sku_size = "Free"
-  
-  depends_on = [
-    azurerm_resource_group.rg_static
-  ]
+  name                = "prod-streamline"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_tier            = "Free"
+  sku_size            = "Free"
 }
 
-resource "azurerm_resource_group" "rg_functions" {
-  name     = "functions-rg"
-  location = "eastus2"
+resource "azurerm_cosmosdb_account" "cosmosdb" {
+name                      = "prod-streamline-cosmosdb"
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
+  offer_type                = "Standard"
+  kind                      = "MongoDB"
+  mongo_server_version = "4.2"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+  capabilities {
+    name = "EnableServerless"
+  }
+
+  backup {
+    type = "Continuous"
+  }
+}
+
+
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = "prod-streamline-workspace"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "log" {
+  name                = "prod-streamline-insights"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.log.id
+  application_type    = "web"
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.log.instrumentation_key
+  sensitive = true
+}
+
+output "app_id" {
+  value = azurerm_application_insights.log.app_id
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "streamlinefiles"
-  resource_group_name      = azurerm_resource_group.rg_functions.name
-  location                 = azurerm_resource_group.rg_functions.location
+  name                     = "prodstreamlinefiles"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  depends_on = [
-    azurerm_resource_group.rg_functions
-  ]
 }
 
-resource "azurerm_service_plan" "ASP-functionsrg-a025" {
-  name                = "dev-streamline-serv-plan"
-  location            = azurerm_resource_group.rg_functions.location
-  resource_group_name = azurerm_resource_group.rg_functions.name
-  sku_name = "Y1"
-  os_type = "Linux"
-  depends_on = [
-    azurerm_resource_group.rg_functions
-  ]
+resource "azurerm_storage_account" "func-storage" {
+  name                     = "funcstreamlinestorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_linux_function_app" "func1" {
-  name                       = "dev-streamline-services"
-  resource_group_name = azurerm_resource_group.rg_functions.name
-  location            = azurerm_resource_group.rg_functions.location
+resource "azurerm_service_plan" "service_plan" {
+  count               = 1
+  name                = element(["prod-streamline-serv-plan", "prod-assistant-serv-plan"], count.index)
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Y1"
+  os_type             = "Linux"
+}
 
-  storage_account_name       = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-  service_plan_id            = azurerm_service_plan.ASP-functionsrg-a025.id
-  https_only = true
+locals {
+  MONGO_URI = split("?", azurerm_cosmosdb_account.cosmosdb.primary_mongodb_connection_string)[0]
+  MONGO_PARAMS = split("?", azurerm_cosmosdb_account.cosmosdb.primary_mongodb_connection_string)[1]
+}
+
+resource "azurerm_linux_function_app" "function_apps" {
+  count = 1
+  name  = element(["prod-streamline-services", "prod-assistant-services"], count.index)
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.service_plan[count.index].id
+  storage_account_name       = azurerm_storage_account.func-storage.name
+  storage_account_access_key = azurerm_storage_account.func-storage.primary_access_key
+  https_only                 = true
+  webdeploy_publish_basic_authentication_enabled = true
 
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "node",
-    "AZURE_SERVICE_BUS_CONNECTION_STRING" = azurerm_servicebus_namespace.sbus.default_primary_connection_string,
-    "AZURE_STORAGE_CONNECTION_STRING" = azurerm_storage_account.storage.primary_connection_string,
-    "MONGO_URI"                          = local.MONGO_URI,
-    "MONGO_PARAMS"                       = local.MONGO_PARAMS,
-    "JWT_SECRET": "UbF6O#71K5(",
-    "JWT_RESET_PASSWORD_SECRET": "UbF6O#71K5(2",
-    "FRONTEND_URL": "streamline.hml-tech4h.com.br",
-    "EMAIL_ACCOUNT": "noreply@hml-tech4h.com.br",
-    "SENDGRID_API_KEY": "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",
-    "SENTRY_DSN" = "",
-    "FRONTEND_URL": "streamline.hml-tech4h.com.br",
-    "LOGGING": "false",
-    NODE_ENV = "development"
+    "FUNCTIONS_WORKER_RUNTIME" = element(["node", "python"], count.index)
+    "APPINSIGHTS_INSTRUMENTATIONKEY"       = azurerm_application_insights.log.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.log.connection_string
+    "AzureWebJobsStorage"      = azurerm_storage_account.func-storage.primary_connection_string
+    "AZURE_SERVICE_BUS_CONNECTION_STRING" = azurerm_servicebus_namespace.sbus.default_primary_connection_string
+    "AZURE_STORAGE_CONNECTION_STRING"     = azurerm_storage_account.storage.primary_connection_string
+    "MONGO_URI"                           = local.MONGO_URI
+    "MONGO_PARAMS"                        = local.MONGO_PARAMS
+    "JWT_SECRET"                          =var.JWT_SECRET
+    "JWT_RESET_PASSWORD_SECRET"           =var.JWT_RESET_PASSWORD_SECRET
+    "FRONTEND_URL"                        =var.FRONTEND_URL
+    "EMAIL_ACCOUNT"                       =var.EMAIL_ACCOUNT
+    "SENDGRID_API_KEY"                    =var.SENDGRID_API_KEY
+    "LOGGING"                             = "false"
+    "NODE_ENV"                            =var.NODE_ENV
+    "SENTRY_DNS"                          = ""
+    "DISCORD_WEBHOOK_URL"                 =var.DISCORD_WEBHOOK_URL
   }
+
   site_config {
     cors {
-      allowed_origins     = [azurerm_static_web_app.static.default_host_name, "streamline.hml-tech4h.com.br"]
+      allowed_origins     = [var.FRONTEND_URL, azurerm_static_web_app.static.default_host_name]
       support_credentials = true
     }
     application_stack {
-      node_version = "20"
+      node_version   = count.index == 0 ? "20" : null
+      python_version = count.index == 1 ? "3.12" : null
     }
+
   }
-  depends_on = [
-    azurerm_service_plan.ASP-functionsrg-a025,
-    azurerm_storage_account.storage,
-    azurerm_servicebus_namespace.sbus,
-    azurerm_static_web_app.static,
-  ]
-}
 
-resource "azurerm_storage_account" "py_storage" {
-  name                     = "assistantstorage"
-  resource_group_name      = azurerm_resource_group.rg_functions.name
-  location                 = azurerm_resource_group.rg_functions.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  depends_on = [
-    azurerm_resource_group.rg_functions
-  ]
-}
-
-resource "azurerm_service_plan" "ASP-functionsrg-a026" {
-  name                = "dev-assistant-serv-plan"
-  location            = azurerm_resource_group.rg_functions.location
-  resource_group_name = azurerm_resource_group.rg_functions.name
-  sku_name = "Y1"
-  os_type = "Linux"
-  depends_on = [
-    azurerm_resource_group.rg_functions
-  ]
-}
-
-resource "azurerm_linux_function_app" "func2" {
-  name                       = "dev-assistant-services"
-  resource_group_name = azurerm_resource_group.rg_functions.name
-  location            = azurerm_resource_group.rg_functions.location
-
-  storage_account_name       = azurerm_storage_account.py_storage.name
-  storage_account_access_key = azurerm_storage_account.py_storage.primary_access_key
-  service_plan_id            = azurerm_service_plan.ASP-functionsrg-a026.id
-  https_only = true
-
-  app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "python",
-    "AZURE_SERVICE_BUS_CONNECTION_STRING" = azurerm_servicebus_namespace.sbus.default_primary_connection_string,
-    "AZURE_STORAGE_CONNECTION_STRING" = azurerm_storage_account.py_storage.primary_connection_string,
-    "JWT_SECRET": "UbF6O#71K5(",
-    "FRONTEND_URL": "streamline.hml-tech4h.com.br",
-    "LOGGING": "false",
-    NODE_ENV = "development"
-  }
-  site_config {
-    cors {
-      allowed_origins     = ["streamline.hml-tech4h.com.br"]
-      support_credentials = true
-    }
-    application_stack {
-      python_version = "3.12"
-    }
-  }
-  depends_on = [
-    azurerm_service_plan.ASP-functionsrg-a026,
-    azurerm_storage_account.py_storage,
+  depends_on = [ 
+    azurerm_application_insights.log, 
+    azurerm_cosmosdb_account.cosmosdb, 
+    azurerm_servicebus_namespace.sbus, 
+    azurerm_storage_account.storage, 
+    azurerm_storage_account.func-storage,
+    azurerm_static_web_app.static
   ]
 }
