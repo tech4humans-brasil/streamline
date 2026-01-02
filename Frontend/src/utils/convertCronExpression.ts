0ea60a18124eece4ff229ce78323d5eef3ccff92
João Guilherme F da Silva <joao.ferreira@tech4h.com.br>
2026-01-02 10:53:40 -0300

    fix: Correct schedule completion condition and enhance cron expression handling
    - Updated the condition for marking a schedule as inactive to ensure it only deactivates when the count of successful repeats meets or exceeds the scheduled repeats.
    - Added support for parsing cron expressions with intervals in hours, improving the flexibility of the scheduling functionality.

--- Frontend/src/utils/convertCronExpression.ts
+++ Frontend/src/utils/convertCronExpression.ts
@@ -40,6 +40,15 @@ export function convertFromCron(cron: string) {
     };
   }
 
+  if (hour.includes("/")) {
+    return {
+      interval: Number(hour.split("/")[1]),
+      schedule: "hour",
+      time: `${formatMinute(minute)}:00`,
+      day: "*"
+    };
+  }
+
   if (hour !== "*") {
     return {
       interval: Number(hour),
